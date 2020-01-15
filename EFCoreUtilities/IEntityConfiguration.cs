1131ddab9daf59a90134c6f488383c715d65fc80
Mark Olbert <mark@arcabama.com>
2020-01-15 15:23:12 -0800

    trying to sync stuff...

--- EFCoreUtilities/IEntityConfiguration.cs
+++ EFCoreUtilities/IEntityConfiguration.cs
@@ -5,6 +5,7 @@ namespace J4JSoftware.EFCoreUtilities
     public interface IEntityConfiguration
     {
         void Configure( ModelBuilder builder );
+        string Ralph { get; }
     }
 
     public interface IEntityConfiguration<TEntity> : IEntityConfiguration
